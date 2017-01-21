2986d09d08656abc15b41f62129106adedb3c953
kaushal197 <kaushal197@gmail.com>
2017-01-21 04:24:14 -0500

    Figured out pyrebase, but don't know how to print into flask without causing a 500 error. We can store images and all that fun stuff Ari

--- firebase.py
+++ firebase.py
@@ -7,7 +7,8 @@ config = {
     "apiKey": "AIzaSyDPxaPjaztIPoI_Hb6YiDGv8KXWkwoHHz8",
     "authDomain": "doggone-8d332.firebaseapp.com",
     "databaseURL": "https://doggone-8d332.firebaseio.com",
-    "storageBucket": "doggone-8d332.appspot.com"
+    "storageBucket": "doggone-8d332.appspot.com",
+    "messagingSenderId": "430271372447"
   }
 
 #initialize firebase with config and pyrebase
@@ -22,9 +23,15 @@ app = Flask(__name__)
 #index page
 @app.route('/')
 def index():
-    print(db)
-    return 'ass'
-
+    #data = {"id": "1", "type": "found", "breed": "", "location": "Toronto"}
+    #db.push(data)
+    all_data = db.get()
+    data = []
+    for piece in all_data.each():
+        data.append(piece.key())
+        data.append(piece.val())
+    return print(data)
+#
 #Data page
 @app.route('/data')
 def names():
